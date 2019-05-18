97348e99b825b424addf65b8793f1faea3e7810a
Bailie Wu <wubailie@gmail.com>
2019-05-18 13:39:04 -0400

    lolcat translation

--- contentscript.js
+++ contentscript.js
@@ -6,3 +6,15 @@ for (let i = 0, l = images.length; i < l; i++) {
   images[i].src =
     'https://placekitten.com/' + images[i].width + '/' + images[i].height
 }
+
+// let all = document.querySelectorAll('body')
+
+let doc = document.getElementsByTagName('*')
+for (let i = 0; i < doc.length; i++) {
+  doc.item(i).innerHTML = doc
+    .item(i)
+    .innerHTML.replace('Lorem', 'ASDKJNSAD')
+    .replace('Where does it come from?', '2')
+}
+
+// console.log(all)
